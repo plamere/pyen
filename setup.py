62b02659c890069a7ddc74356c2e1cf7cf5cdba8
Paul Lamere <Paul.Lamere@gmail.com>
2013-10-20 11:27:35 -0400

    Bumped version number. Fixed repos link.

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from distutils.core import setup
 
 setup(
     name='pyen',
-    version='1.0', 
+    version='1.1', 
     description='simple client for The Echo Nest',
     author="@plamere",
     author_email="paul@echonest.com",
-    url='http://github.com/echonest/pyen',
+    url='http://github.com/plamere/pyen',
     py_modules=['pyen'],)
