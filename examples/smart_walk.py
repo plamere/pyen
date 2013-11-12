2fa358681f43d2e778ee053f991fb64c0ffcdea2
Paul Lamere <Paul.Lamere@gmail.com>
2013-11-12 06:20:52 -0500

    Added smart walker example

---
+++ examples/smart_walk.py
@@ -0,0 +1,20 @@
+import pyen
+
+en = pyen.Pyen()
+
+queue = []
+done = set()
+queue.append('ARH6W4X1187B99274F')
+
+while len(queue) > 0:
+    cid = queue.pop(0)
+    if cid not in done:
+        response = en.get('artist/similar', id=cid, bucket='artist_location')
+        done.add(cid)
+
+        for artist in response['artists']:
+            if not artist['id'] in done and not artist['id'] in queue:
+                if 'artist_location' in artist:
+                    location = artist['artist_location']['location']
+                    print artist['id'], artist['name'], location
+                    queue.append(artist['id'])
