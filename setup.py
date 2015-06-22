de69aea07b2bec332dd6d79b411dce68c4dc3503
Paul Lamere <Paul.Lamere@gmail.com>
2015-06-22 06:43:56 +0200

    Removed debugging info with a utf issue

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pyen',
-    version='2.3', 
+    version='2.3.1', 
     description='simple client for The Echo Nest',
     author="@plamere",
     author_email="paul@echonest.com",
